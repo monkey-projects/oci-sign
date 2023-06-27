92639ce7bb1126e02690e2a682d7ff4e68bc9b0a
Wout Neirynck <wout@monkey-projects.be>
2023-06-27 16:48:27 +0200

    Signing algorithm

--- src/monkey/oci/sign.clj
+++ src/monkey/oci/sign.clj
@@ -1,7 +1,57 @@
 (ns monkey.oci.sign
-  (:require [buddy.sign.compact :as c]))
+  (:require [clojure.string :as cs]
+            [clojure.spec.alpha :as s]
+            [buddy.sign.compact :as c])
+  (:import [java.net URI URLEncoder]
+           java.nio.charset.StandardCharsets))
+
+(def private-key? (partial instance? java.security.PrivateKey))
+
+(s/def ::tenancy-ocid string?)
+(s/def ::user-ocid string?)
+(s/def ::key-fingerprint string?)
+(s/def ::private-key private-key?)
+
+(s/def ::config (s/keys :req-un [::tenancy-ocid
+                                 ::user-ocid
+                                 ::key-fingerprint
+                                 ::private-key]))
+
+(defn key-id [conf]
+  (->> [:tenancy-ocid :user-ocid :key-fingerprint]
+       (map (partial get conf))
+       (cs/join "/")))
+
+(defn- url-encode [s]
+  (URLEncoder/encode s StandardCharsets/UTF_8))
+
+(defn sign-headers
+  "Builds signing headers from the request"
+  [{:keys [url method] :as req}]
+  (let [uri (URI/create url)]
+    {"date" (first (get-in req [:headers "date"]))
+     "(request target)" (url-encode (str (name method) " " (.getPath uri)))
+     "host" (.getHost uri)}))
 
 (defn sign
-  "Signs a request by calculcating a signature based on the given config."
-  [conf req]
-  )
+  "Signs a request by calculating a signature based on the given config."
+  [conf headers]
+  (let [h (->> (keys headers)
+               (cs/join " "))
+        v (->> headers
+               (reduce-kv (fn [r k v]
+                            (conj r (str k ": " v)))
+                          [])
+               (cs/join "\n"))
+        signature (c/sign v (:private-key conf) {:alg :ps256})
+        m {"headers" h
+           "keyId" (key-id conf)
+           "algorithm" "rsa-sha256"
+           "signature" signature
+           "version" "1"}]
+    (->> m
+         (reduce-kv (fn [r k v]
+                      (conj r (str k "=\"" v "\"")))
+                    [])
+         (cs/join ",")
+         (str "Signature "))))
--- test/monkey/oci/test/sign_test.clj
+++ test/monkey/oci/test/sign_test.clj
@@ -1,8 +1,68 @@
 (ns monkey.oci.test.sign-test
-  (:require [clojure.test :refer :all]
-            [monkey.oci.sign :as sut]))
+  (:require [buddy.core.keys.pem :as pem]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [monkey.oci.sign :as sut])
+  (:import java.net.URI
+           java.util.Optional
+           java.util.function.Supplier
+           com.oracle.bmc.http.signing.SigningStrategy
+           [com.oracle.bmc.http.signing.internal KeySupplier RequestSignerImpl]))
+
+(defn load-privkey []
+  (with-open [r (-> (io/resource "test/test.key")
+                    (io/reader))]
+    (pem/read-privkey r nil)))
+
+(defn- as-key-supplier [f]
+  (reify KeySupplier
+    (supplyKey [_ _]
+      (Optional/of (f)))))
+
+(defn- as-supplier [f]
+  (reify Supplier
+    (get [_]
+      (f))))
+
+(defn sign-java
+  "Generates a signature using the Oracle Java library.  This is used
+   as a reference to compare our own signatures against."
+  [conf {:keys [url method headers]}]
+  (let [signer (RequestSignerImpl. (as-key-supplier (constantly (:private-key conf)))
+                                   SigningStrategy/EXCLUDE_BODY
+                                   (as-supplier (constantly (sut/key-id conf))))]
+    (.signRequest
+     signer
+     (URI/create url)
+     (name method)
+     headers
+     nil)))
+
+(deftest privkey
+  (testing "can load"
+    (is (some? (load-privkey)))))
+
+(deftest key-id
+  (testing "constructs key id from config values"
+    (is (= "a/b/c" (sut/key-id {:tenancy-ocid "a"
+                                :user-ocid "b"
+                                :key-fingerprint "c"})))))
 
 (deftest sign
-  (testing "generates signature"
-    (is (some? (sut/sign nil nil)))))
+  (let [conf {:private-key (load-privkey)
+              :user-ocid "user-ocid"
+              :tenancy-ocid "tenancy-ocid"
+              :key-fingerprint "fingerprint"}
+        req  {:url "http://localhost/test"
+              :method :get
+              :headers {"date" ["Tue, 27 Jun 2023 14:59:40 GMT"]}}]
+    
+    (testing "generates signature"
+      (is (string? (sut/sign conf (sut/sign-headers req)))))
 
+    (testing "signature matches reference"
+      (let [r (sign-java conf req)
+            auth (get r "authorization")]
+        (is (not-empty r))
+        (is (string? auth))
+        (is (= auth (sut/sign conf (sut/sign-headers req))))))))

f9d3022e9cc618f3f40d0d98fdde54f32892405d
Wout Neirynck <wout@monkey-projects.be>
2023-06-28 13:31:38 +0200

    Added logging

--- src/monkey/oci/sign.clj
+++ src/monkey/oci/sign.clj
@@ -3,7 +3,9 @@
             [clojure.spec.alpha :as s]
             [buddy.sign.compact :as c])
   (:import [java.net URI URLEncoder]
-           java.nio.charset.StandardCharsets))
+           java.util.Base64
+           java.nio.charset.StandardCharsets
+           com.oracle.bmc.http.signing.internal.SignatureSigner))
 
 (def private-key? (partial instance? java.security.PrivateKey))
 
@@ -17,21 +19,34 @@
                                  ::key-fingerprint
                                  ::private-key]))
 
+(def charset StandardCharsets/UTF_8)
+
 (defn key-id [conf]
   (->> [:tenancy-ocid :user-ocid :key-fingerprint]
        (map (partial get conf))
        (cs/join "/")))
 
 (defn- url-encode [s]
-  (URLEncoder/encode s StandardCharsets/UTF_8))
+  (URLEncoder/encode s charset))
 
 (defn sign-headers
   "Builds signing headers from the request"
   [{:keys [url method] :as req}]
   (let [uri (URI/create url)]
     {"date" (first (get-in req [:headers "date"]))
-     "(request target)" (url-encode (str (name method) " " (.getPath uri)))
+     ;; TODO Add query string
+     "(request-target)" (str (name method) " " (.getRawPath uri))
+     ;; TODO Add port
      "host" (.getHost uri)}))
+
+(defn- generate-signature [s pk]
+  #_(c/sign s pk {:alg :ps256})
+  (let [signer (SignatureSigner.)
+        enc (Base64/getEncoder)]
+    (->> (.sign signer pk (.getBytes s charset) "SHA256withRSA")
+         (.encode enc)
+         (String.))))
+        
 
 (defn sign
   "Signs a request by calculating a signature based on the given config."
@@ -43,7 +58,7 @@
                             (conj r (str k ": " v)))
                           [])
                (cs/join "\n"))
-        signature (c/sign v (:private-key conf) {:alg :ps256})
+        signature (generate-signature v (:private-key conf))
         m {"headers" h
            "keyId" (key-id conf)
            "algorithm" "rsa-sha256"

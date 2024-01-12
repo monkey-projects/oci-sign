814c9fe86327b79f72e5376d3e2497b667a767d8
Wout Neirynck <wout@monkey-projects.be>
2024-01-12 11:37:20 +0100

    Load build params for integration tests

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,3 +1,18 @@
-(require '[monkey.ci.plugin.clj :as p])
+(ns sign.build
+  (:require [monkey.ci.build
+             [api :as api]
+             [core :as bc]]
+            [monkey.ci.plugin.clj :as p]))
 
-(p/deps-library)
+;; A bit more complicated because we need to get some build params for
+;; integration tests
+(defn test [ctx]
+  (let [props (-> (api/build-params ctx)
+                  (select-keys ["TENANCY_OCID" "USER_OCID" "KEY_FINGERPRINT"
+                                "PRIVATE_KEY" "REGION"]))]
+    (-> (p/deps-test {})
+        (update :container/env merge props))))
+
+(bc/defpipeline build
+  [test
+   (p/deps-publish {})])
